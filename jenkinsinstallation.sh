086eda6c01051fad265f89dae8af0918a648b429
Bharath Shekar <53362864+BharathShekar@users.noreply.github.com>
2022-01-27 10:19:49 +0530

    Create jenkinsinstallation.sh

---
+++ jenkinsinstallation.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt install -y openjdk-11-jre-headless
+wget -qO - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo apt-get upgrade
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get -y install jenkins
